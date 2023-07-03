a51fb15ed43b6f2f5244281a2493bd761692a8ee
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-07-03 11:34:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 //your JS code here. If required.
+function setCookie(name, value, days) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${encodeURIComponent(value)};expires=${expires.toUTCString()}`;
+}
+
+function getCookie(name) {
+  const cookieName = `${name}=`;
+  const cookieArray = document.cookie.split(';');
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return decodeURIComponent(cookie.substring(cookieName.length));
+    }
+  }
+  return null;
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const fontSizeInput = document.getElementById('fontsize');
+  const fontColorInput = document.getElementById('fontcolor');
+
+  const fontSize = fontSizeInput.value;
+  const fontColor = fontColorInput.value;
+
+  setCookie('fontSize', fontSize, 30);
+  setCookie('fontColor', fontColor, 30);
+
+  applyUserPreferences();
+}
+
+function applyUserPreferences() {
+  const fontSize = getCookie('fontSize');
+  const fontColor = getCookie('fontColor');
+
+  if (fontSize) {
+    document.body.style.fontSize = fontSize + 'px';
+  }
+  if (fontColor) {
+    document.body.style.color = fontColor;
+  }
+}
+
+const preferencesForm = document.getElementById('preferences-form');
+preferencesForm.addEventListener('submit', handleFormSubmit);
+
+applyUserPreferences();
